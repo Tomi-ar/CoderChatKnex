57455ce0db06a09b87f446d94f7634aff7199640
Tomi-ar <tomarrillaga@gmail.com>
2022-03-20 22:51:38 -0300

    Clase44 con Graphql v1.0

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express();
 const router = require("./src/routes/productos");
 const { engine } = require('express-handlebars');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 // Socket
 require('./src/routes/socket')
 
@@ -30,6 +33,102 @@ app.engine(
     })
 )
 
+const graphSchema = buildSchema(`
+    type Producto{
+        id: String!,
+        nombre: String!,
+        precio: Float!,
+        thumb: String!,
+        role: String,
+    }
+    
+    input ProductoInput{
+        nombre: String!,
+        precio: Float!,
+        thumb: String!,
+        role: String,
+    }
+
+    type Query{
+        getAll: [Producto],
+        getById(id: String!): Producto,
+    }
+
+    type Mutation{
+        save(input: ProductoInput): Producto,
+        updateId(id: String!, input: ProductoInput): Producto,
+        deleteId(id: String!): Producto,
+    }
+`)
+
+const MongoClient = require('./src/models/config/MongoDB')
+const conectar = new MongoClient().connect
+const products = require('./src/models/config/MongoSchema');
+
+
+const root = {
+    getAll: async () => {
+        try {
+            let data = await products.find({});
+            return data;
+        } catch (error) {
+            console.log("Problemas con el get "+error);
+        }
+    },
+    getById: async (id) => {
+        try {
+            let data = await products.find({_id: id});
+            return data;
+        } catch (error) {
+            console.log("ID no encontrado "+error);
+        }
+    },
+    save: async (product) => {
+        try {
+            let newProd = {
+                nombre: product.nombre,
+                precio: product.precio,
+                thumb: product.thumb
+            };
+            let data = await new products(newProd).save()
+            return data;
+        } catch (error) {
+            console.log("Problemas con el save "+error);
+        }
+    },
+    deleteId: async (id) => {
+        try {
+            let data = await products.findOneAndRemove({_id: id});
+            return data;
+        } catch (error) {
+            console.log("Problemas con el delete "+error);
+        }
+    },
+    updateId: async (id, product) => {
+        try {
+            let data = products.findOneAndUpdate({_id: id}, product)
+            return data;
+        } catch (error) {
+            console.log("Problemas con el update "+error);
+        }
+    }
+}
+
+app.use('/graphql', graphqlHTTP({
+    schema: graphSchema,
+    rootValue: root,
+    graphiql: true,
+}))
+
 server.listen(port, () => {
     console.log("Server running on "+port);
 })
+
+
+//**************QUERIES */
+// query{
+//     getAll{
+//         nombre
+//         id
+//     }
+// }
